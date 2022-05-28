5d363f73640ce520b7189627bc77f944cdb03323
nattyKhoe <natasha.kusuma@outlook.com>
2022-05-28 23:29:19 +1000

    commit tree fd0fcd649bcdcfa7e834d47fc22e57abcf042955

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat (name){
+    cats.push (name);
+}
+function destructivelyPrependCat (name){
+    cats.unshift (name);
+}
+destructivelyRemoveLastCat = () =>{cats.pop()};
+destructivelyRemoveFirstCat = () =>{cats.shift()};
+appendCat = (name) =>{return [...cats, name]};
+prependCat =(name) =>{return [name, ...cats]};
+removeLastCat =() =>{return cats.slice(0,-1)};
+removeFirstCat =() =>{return cats.slice(1)};
